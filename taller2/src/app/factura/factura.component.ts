48bb7a469e38abfa700cabd250d19f3abf4867e1
armero98 <armerosteven@gmail.com>
2021-07-21 21:32:44 -0500

    cambios faltantes

--- taller2/src/app/factura/factura.component.ts
+++ taller2/src/app/factura/factura.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacturaComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
 
+  title = 'taller2';
+  nombreProducto=""
+  arrayItems = []
+  arrayFacturas = []
+  deshabilitado = true
+  saveFactura = true
+  siHay = true
+  nombre = "";
+  precio = 0;
+
+
+    agregarItem(){ 
+      try {
+        if(this.nombre != ""){
+        this.agregarItem.push(cajaitem)
+      }catch (e) {
+        console.log('no hay item para agregar=>', e)
+      }
+    }
+
+    guardarFactura(){ 
+      try {
+        if(this.arrayItems.length > 1){
+          this.saveFactura =! this.saveFactura
+          this.arrayFacturas.push;}
+        }catch (e) {
+        console.log('no hay factura que guardar', e)
+      } 
+    }
+
+ 
 }
